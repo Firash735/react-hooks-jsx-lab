c790b5e08a360cde85f30f2f191474baff2eb238
Abdisharif <sharifabdi735@gmail.com>
2024-08-06 14:25:13 +0300

    finished

--- src/__tests__/About.test.js
+++ src/__tests__/About.test.js
@@ -3,43 +3,30 @@ import { render, screen } from "@testing-library/react";
 import { image } from "../data/data";
 import About from "../components/About";
 
-test("renders a <div> with the ID of 'about'", () => {
+test("renders a <div> with the correct ID", () => {
   const { container } = render(<About />);
-
-  const about = container.querySelector("#about");
-  expect(about).toBeInTheDocument();
+  expect(container.querySelector("#about")).toBeInTheDocument();
 });
 
-test("the <div> has three child elements", () => {
+test("renders a <div> with three children", () => {
   const { container } = render(<About />);
-
-  const about = container.querySelector("#about");
-  expect(about.children).toHaveLength(3);
+  expect(container.querySelector("#about").children).toHaveLength(3);
 });
 
 test("renders a <h2> with the text 'About Me'", () => {
-  render(<About />);
-
-  // find an element with the text content "About Me"
-  const h2 = screen.queryByText("About Me");
-
-  // check that it's a <h2> element
-  expect(h2.tagName).toBe("H2");
+  const { container } = render(<About />);
+  const h2 = container.querySelector("h2");
+  expect(h2.textContent).toEqual("About Me");
 });
 
 test("renders a <p> element", () => {
   const { container } = render(<About />);
-
-  const p = container.querySelector("p");
-  expect(p).toBeInTheDocument();
+  expect(container.querySelector("p")).toBeInTheDocument();
 });
 
 test("renders a <img> element with the correct attributes", () => {
   render(<About />);
-
-  // Find an element with the "alt" attribute set to "I made this"
   const img = screen.queryByAltText("I made this");
-
   expect(img).toBeInTheDocument();
-  expect(img).toHaveAttribute("src", image);
+  expect(img.src).toEqual(image);
 });
